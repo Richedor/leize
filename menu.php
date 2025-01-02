7b9ae2317094aba00d2d4737a7e3e6e9b1f98be1
Richedor <77790992+Richedor@users.noreply.github.com>
2025-01-02 23:47:22 +0100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <ul>
+        <li><a href="gestionVehicule.htm">Accueil</a></li>
+        <li><a href="prendreUnVehiculeEtape1.php">Prendre un véhicule</a></li>
+        <li><a href="selectionVehiculeRetour.php">Retour de véhicule</a></li>
+        <!-- Ajoutez d'autres liens de menu ici -->
+    </ul>
+</nav>
